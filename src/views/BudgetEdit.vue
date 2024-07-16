4ab7cf3bb5e3deb2a03ebfff63752421b72c15bb
firdawawa <firdaardana@student.ub.ac.id>
2024-07-16 11:26:02 +0700

    todo list

--- src/views/BudgetEdit.vue
+++ src/views/BudgetEdit.vue
@@ -1,53 +1,149 @@
 <template>
-
-    <div class="container mt-24">
-            <div class="card-table">
-                <div class="block max-w p-6 m-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300">
-                    <div class="card-header">
-                        <h4 class="font-bold text-xl">
-                            Edit Data Budget List
-                        </h4>
-                    </div>
-                    <div class="card-body">
-    
-                        <!-- <ul class="alert" v-if="Object.keys(this.errorList).length > 0">
-                            <li class="mb-0 ms-3 " v-for="(error, index) in this.errorList" :key="index">
-                                {{ error[0] }}
-                            </li>
-                        </ul> -->
-    
-                        <div class="block max-w p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300">
-                            <div class="relative content-center overflow-x-auto">
-                                <div class="col-name mb-3">
-                                    <label for="">Date</label>
-                                    <input type="text" class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800" placeholder="Date" required />
-                                </div>
-                                <div class="col-number mb-3">
-                                    <label for="">Criteria</label>
-                                    <input type="text" class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800" placeholder="Category" required />
-                                </div>
-                                <div class="col-number mb-3">
-                                    <label for="">Category</label>
-                                    <input type="text" class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800" placeholder="Criteria" required />
-                                </div>
-                                <div class="col-number mb-3">
-                                    <label for="">Nominal</label>
-                                    <input type="text" class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800" placeholder="Rp nominal" required />
-                                </div>
-                                <div class="col-number mb-3">
-                                    <button type="button" class="mt-6 w-40 text-white bg-primary hover:bg-pink-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md float-end px-5 py-3 me-2 mb-2 dark:bg-primary dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-900">
-                                        Save
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="container mt-24">
+    <div class="card-table">
+      <div
+        class="block max-w p-6 m-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300"
+      >
+        <div class="card-header">
+          <h4 class="font-bold text-xl">Edit Data Budget List</h4>
         </div>
-    
-    </template>
+        <div class="card-body">
+          <!-- <ul class="alert" v-if="Object.keys(this.errorList).length > 0">
+                          <li class="mb-0 ms-3 " v-for="(error, index) in this.errorList" :key="index">
+                              {{ error[0] }}
+                          </li>
+                      </ul> -->
+
+          <div
+            class="block max-w p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300"
+          >
+            <div class="relative content-center overflow-x-auto">
+              <div class="col-name mb-3">
+                <label for="">Date</label>
+                <input
+                  type="date"
+                  v-model="model.data.date"
+                  class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800"
+                  placeholder="Date"
+                  required
+                />
+              </div>
+              <div class="col-number mb-3">
+                <label for="">Criteria</label>
+                <input
+                  type="text"
+                  v-model="model.data.criteria"
+                  class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800"
+                  placeholder="Category"
+                  required
+                />
+              </div>
+              <div class="col-number mb-3">
+                <label for="">Category</label>
+                <input
+                  type="text"
+                  v-model="model.data.category"
+                  class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800"
+                  placeholder="Criteria"
+                  required
+                />
+              </div>
+              <div class="col-number mb-3">
+                <label for="">Nominal</label>
+                <input
+                  type="number"
+                  v-model="model.data.nominal"
+                  class="block mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-400 dark:text-grey-800"
+                  placeholder="Rp nominal"
+                  required
+                />
+              </div>
+              <div class="col-number mb-3">
+                <button
+                  type="button"
+                  @click="updateData"
+                  class="mt-6 w-40 text-white bg-primary hover:bg-pink-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md float-end px-5 py-3 me-2 mb-2 dark:bg-primary dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-900"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
+import axios from 'axios';
 
+export default {
+  name: 'NotesEdit',
+  data() {
+    return {
+      id: '',
+      errorList: '',
+      model: {
+        data: {
+          date: '',
+          criteria: '',
+          category: '',
+          nominal: '',
+        },
+      },
+    };
+  },
+  mounted() {
+    this.id = this.$route.params.id;
+    this.getData(this.$route.params.id);
+  },
+  methods: {
+    getData(id) {
+      axios
+        .get(`http://192.168.5.32:8000/api/budget/${id}/edit`)
+        .then(res => {
+          this.model.data = res.data.budget;
+        })
+        .catch(function (error) {
+          if (error.response) {
+            if (error.response.status == 404) {
+              alert(error.response.data.message);
+            }
+          }
+        });
+    },
+
+    updateData() {
+      var $this = this;
+      axios
+        .put(
+          `http://192.168.5.32:8000/api/budget/${this.id}/edit`,
+          this.model.data,
+        )
+        .then(res => {
+          console.log(res.data);
+          alert(res.data.message);
+
+          this.model.data = {
+            date: '',
+            criteria: '',
+            category: '',
+            nominal: '',
+          };
+        })
+        .catch(function (error) {
+          if (error.response) {
+            if (error.response.status == 422) {
+              this.errorList = error.response.data.errors;
+            }
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log('Error', error.message);
+          }
+        });
+    },
+  },
+};
 </script>
--- src/views/BudgetView.vue
+++ src/views/BudgetView.vue
@@ -1,61 +1,119 @@
 <template>
-    <div class="mt-20">
-        <div class="card-table">
-            <div class="block max-w p-6 m-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300">
-                <div class="card-header">
-                    <h4 class="font-bold text-xl">
-                        Budget List
-                        <RouterLink to="budget/create">
-                            <button type="button" class="text-white bg-primary hover:bg-pink-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 -mt-2 dark:bg-primary dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-900 float-end">Add Data</button>
-                        </RouterLink>
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <div class="block max-w p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300">
-                        <div class="relative content-center overflow-x-auto">
-                            <table class="w-full text-sm text-left rtl:text-right text-gray-900 dark:text-gray-900">
-                                <thead class="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-primary dark:text-white">
-                                    <tr>
-                                        <th scope="col" class="px-6 py-5">
-                                            Date
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Criteria
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Category
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Nominal
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Action
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-    
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="mt-20">
+    <div class="card-table">
+      <div
+        class="block max-w p-6 m-1 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300"
+      >
+        <div class="card-header">
+          <h4 class="font-bold text-xl">
+            Budget List
+            <RouterLink to="budget/create">
+              <button
+                type="button"
+                class="text-white bg-primary hover:bg-pink-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 -mt-2 dark:bg-primary dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-900 float-end"
+              >
+                Add Data
+              </button>
+            </RouterLink>
+          </h4>
         </div>
+        <div class="card-body">
+          <div
+            class="block max-w p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-300"
+          >
+            <div class="relative content-center overflow-x-auto">
+              <table
+                class="w-full text-sm text-left rtl:text-right text-gray-900 dark:text-gray-900"
+              >
+                <thead
+                  class="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-primary dark:text-white"
+                >
+                  <tr>
+                    <th scope="col" class="px-6 py-5">Date</th>
+                    <th scope="col" class="px-6 py-3">Criteria</th>
+                    <th scope="col" class="px-6 py-3">Category</th>
+                    <th scope="col" class="px-6 py-3">Nominal</th>
+                    <th scope="col" class="px-6 py-3">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(data, i) in datas" :key="i">
+                    <td class="px-6 py-4">
+                      {{ data.date }}
+                    </td>
+                    <td class="px-6 py-4">
+                      {{ data.criteria }}
+                    </td>
+                    <td class="px-6 py-4">
+                      {{ data.category }}
+                    </td>
+                    <td class="px-6 py-4">Rp{{ data.nominal }}</td>
+
+                    <td class="px-6 py-4">
+                      <RouterLink :to="{ path: `/budget/${data.id}/edit` }">
+                        <button
+                          type="button"
+                          class="text-white bg-primary hover:bg-pink-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 -mt-2 dark:bg-primary dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-900"
+                        >
+                          Edit
+                        </button>
+                      </RouterLink>
+                      <button
+                        type="button"
+                        @click="deleteData(data.id)"
+                        class="text-white bg-red-600 hover:bg-red-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 -mt-2 dark:bg-red-600 dark:hover:bg-red-900 focus:outline-none dark:focus:ring-pink-900"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </template>
-    
-    <script>
-    
-    export default {
-        name: 'phone',
-        data(){
-            return {
-                students: []
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'phone',
+  data() {
+    return {
+      datas: [],
+    };
+  },
+  mounted() {
+    console.log('Success');
+    this.getData();
+  },
+  methods: {
+    getData() {
+      axios.get('http://192.168.5.32:8000/api/budget').then(res => {
+        this.datas = res.data.budget;
+        console.log(res.data);
+      });
+    },
+    deleteData(id) {
+      if (confirm('Are you sure to delete this?')) {
+        axios
+          .delete(`http://192.168.5.32:8000/api/budget/${id}/delete`)
+          .then(res => {
+            alert(res.data.message);
+            this.getData();
+          })
+          .catch(function (error) {
+            if (error.response) {
+              if (error.response.status == 404) {
+                alert(error.response.data.message);
+              }
             }
-        },
-        mounted(){
-            console.log('Success')
-        }
-    }
-    </script>
+          });
+      }
+    },
+  },
+};
+</script>
